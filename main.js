008f9721e5cecff56f8835560c3a7517c54544fd
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-30 16:32:40 +0300

    lesson Array methods

--- main.js
+++ main.js
@@ -1,46 +1,135 @@
-// && = va // || = yoki
+// // && = va // || = yoki
 
-// let a = 10
-// let b = 16
+// // let a = 10
+// // let b = 16
 
-// if (a === 10 && b === 15) {
-//     // true
-//     console.log(true);
+// // if (a === 10 && b === 15) {
+// //     // true
+// //     console.log(true);
+// // } else {
+// //     //  false
+// //     console.log(false);
+// // }
+
+
+// // if (a === 10 || b === 15) {
+// //     console.log(true);
+// // } else {
+// //     console.log(false);
+// // }
+
+
+// let login = prompt("create new login")
+// let password = prompt("create new password")
+
+// if (login.trim() !== '' && password.trim() !== '') {
+//     alert('true')
+
+//     let l = prompt('write your login')
+//     let p = prompt('write your password')
+
+//     if (login === l.trim() && password === p.trim()) {
+//         alert('welcome')
+//     } else {
+//         alert('user is not defined!!!')
+//     }
+
 // } else {
-//     //  false
-//     console.log(false);
+//     alert("false")
 // }
 
 
-// if (a === 10 || b === 15) {
-//     console.log(true);
-// } else {
-//     console.log(false);
-// }
+
+// array / object
 
 
-let login = prompt("create new login")
-let password = prompt("create new password")
+let arr = [
+    "salom",
+    81,
+    true,
+    false,
+    "Xayr",
+    {},
+    {},
+    [],
+    [], [],
+    NaN,
+    null,
+    undefined
+]
 
-if (login.trim() !== '' && password.trim() !== '') {
-    alert('true')
+// console.log(arr);
+// // console.log();
 
-    let l = prompt('write your login')
-    let p = prompt('write your password')
-
-    if (login === l.trim() && password === p.trim()) {
-        alert('welcome')
-    } else {
-        alert('user is not defined!!!')
-    }
-
-} else {
-    alert("false")
-}
-
-console.log(
-    login, password
-);
+// console.log(
+//     arr[3]
+// );
 
 
 
+// console.log(names);
+
+
+// console.log(numbers);
+
+// console.log(names[4]);
+
+
+// let a = "Alex"
+
+// a = "Aleksandr"
+
+// console.log(a);
+
+// names[5] = "Aleksandr"
+
+// console.log(names[5]);
+// console.log(names);
+
+
+
+
+
+// // console.log(numbers);
+
+// let a = numbers.sort((a, b) => a - b)
+// let b = numbers.sort()
+
+// console.log(
+//     a
+// );
+
+// console.log(
+//     names.sort()
+// );
+
+// element qushish
+
+let names = ["Ahmadbek", "Jasur", "Samir", "Javohir", "Ilhom", "Alex", "Alex"]
+let numbers = [1, 515, 5, 1, 5, 1, 51, 6, 51, 6, 51, 1, 651, 6, 51]
+
+// names.unshift('Ahmed')
+// names.unshift('Mehmed')
+
+// names.push("Davlatshoh")
+// names.push("Alisa")
+
+// // elementni uchirish
+
+// names.pop()
+// names.shift()
+
+// qidirish
+
+names.indexOf("Samir")
+// names.find("Samir")
+
+console.log(numbers);
+
+let filteredNumbers = numbers.filter(item => item < 50)
+
+console.log(filteredNumbers);
+
+names.splice(3, 1, "Qodir")
+
+// console.log(names);
