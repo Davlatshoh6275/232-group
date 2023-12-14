e1ce46a2994f6176376c78ab9b22b60481f16ffd
Davlatshoh <davlatshoh6275@gmail.com>
2023-12-14 16:54:51 +0300

    lesson amaliyot

--- main.js
+++ main.js
@@ -171,7 +171,7 @@
 // }
 
 // if(shart) {
-//     true 
+//     true
 // } else if(shart) {
 //     true
 // } else {
@@ -1633,72 +1633,75 @@
 // chizish()
 
 
-const inpTodo = document.querySelector('#todo')
-const addTodo = document.querySelector('#addTodo')
-const todoItems = document.querySelector('.todoItems')
+// const inpTodo = document.querySelector('#todo')
+// const addTodo = document.querySelector('#addTodo')
+// const todoItems = document.querySelector('.todoItems')
 
-let state = {
-    todos: [],
-}
-
-
-
-
-let title = ''
-
-inpTodo.addEventListener('keyup', (e) => {
-    title = e.target.value
-})
-
-let counter = 0
-
-addTodo.addEventListener('click', (e) => {
-    e.preventDefault()
-    let newTodo = {
-        id: counter,
-        title,
-    }
-    let todoHTml = `
-            <div class="item">
-                <h1 class="itemCount" >${newTodo.id}</h1>
-                <p class="itemText">${newTodo.title}</p>
-            </div>
-        `
-
-    todoItems.insertAdjacentHTML('beforeend', todoHTml)
-
-    state.todos.push(newTodo)
-
-
-    counter++
-    newTodo.id = counter
-    localStorage.setItem('todos', JSON.stringify(state.todos))
-    inpTodo.value = ''
-
-    console.log(state);
-
-})
+// let state = {
+//     todos: [],
+// }
 
 
 
-function local() {
 
-    state.todos = JSON.parse(localStorage.getItem('todos') || '[]' ) 
+// let title = ''
 
-    for (let i = 0; i < state.todos.length; i++) {
-        let todoHTml = `
-            <div class="item">
-                <h1 class="itemCount" >${state.todos[i].id}</h1>
-                <p class="itemText">${state.todos[i].title}</p>
-            </div>
-        `
+// inpTodo.addEventListener('keyup', (e) => {
+//     title = e.target.value
+// })
 
-        todoItems.insertAdjacentHTML('beforeend', todoHTml)
+// let counter = 0
 
-    }
-}
+// addTodo.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     let newTodo = {
+//         id: counter,
+//         title,
+//     }
+//     let todoHTml = `
+//             <div class="item">
+//                 <h1 class="itemCount" >${newTodo.id}</h1>
+//                 <p class="itemText">${newTodo.title}</p>
+//             </div>
+//         `
 
-local()
+//     todoItems.insertAdjacentHTML('beforeend', todoHTml)
+
+//     state.todos.push(newTodo)
+
+
+//     counter++
+//     newTodo.id = counter
+//     localStorage.setItem('todos', JSON.stringify(state.todos))
+//     inpTodo.value = ''
+
+//     console.log(state);
+
+// })
+
+
+
+// function local() {
+
+//     state.todos = JSON.parse(localStorage.getItem('todos') || '[]' )
+
+//     for (let i = 0; i < state.todos.length; i++) {
+//         let todoHTml = `
+//             <div class="item">
+//                 <h1 class="itemCount" >${state.todos[i].id}</h1>
+//                 <p class="itemText">${state.todos[i].title}</p>
+//             </div>
+//         `
+
+//         todoItems.insertAdjacentHTML('beforeend', todoHTml)
+
+//     }
+// }
+
+// local()
+
+
+
 
 
 // let btnPlus = document.querySelector('.btnPlus')
@@ -1738,4 +1741,308 @@ local()
 //     localStorage.setItem('counter', JSON.stringify(a))
 // })
 
+// const btn = document.querySelector('.btn')
 
+
+// console.log('sana:', date.getDate());
+// console.log('hours:', date.getHours(), "minutes:", date.getMinutes());
+
+
+
+// btn.addEventListener('click', () => {
+//     let date = new Date()
+//     let hour = date.getHours()
+//     let minute = date.getMinutes()
+//     let seconds = date.getSeconds()
+
+//     console.log(
+//         `${hour}:${minute}:${seconds}`
+//     );
+// })
+
+
+const content = document.querySelector('.content')
+
+
+let arr =
+    [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "username": "Bret",
+            "email": "Sincere@april.biz",
+            "address": {
+                "street": "Kulas Light",
+                "suite": "Apt. 556",
+                "city": "Gwenborough",
+                "zipcode": "92998-3874",
+                "geo": {
+                    "lat": "-37.3159",
+                    "lng": "81.1496"
+                }
+            },
+            "phone": "1-770-736-8031 x56442",
+            "website": "hildegard.org",
+            "company": {
+                "name": "Romaguera-Crona",
+                "catchPhrase": "Multi-layered client-server neural-net",
+                "bs": "harness real-time e-markets"
+            }
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "username": "Antonette",
+            "email": "Shanna@melissa.tv",
+            "address": {
+                "street": "Victor Plains",
+                "suite": "Suite 879",
+                "city": "Wisokyburgh",
+                "zipcode": "90566-7771",
+                "geo": {
+                    "lat": "-43.9509",
+                    "lng": "-34.4618"
+                }
+            },
+            "phone": "010-692-6593 x09125",
+            "website": "anastasia.net",
+            "company": {
+                "name": "Deckow-Crist",
+                "catchPhrase": "Proactive didactic contingency",
+                "bs": "synergize scalable supply-chains"
+            }
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "username": "Samantha",
+            "email": "Nathan@yesenia.net",
+            "address": {
+                "street": "Douglas Extension",
+                "suite": "Suite 847",
+                "city": "McKenziehaven",
+                "zipcode": "59590-4157",
+                "geo": {
+                    "lat": "-68.6102",
+                    "lng": "-47.0653"
+                }
+            },
+            "phone": "1-463-123-4447",
+            "website": "ramiro.info",
+            "company": {
+                "name": "Romaguera-Jacobson",
+                "catchPhrase": "Face to face bifurcated interface",
+                "bs": "e-enable strategic applications"
+            }
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "username": "Karianne",
+            "email": "Julianne.OConner@kory.org",
+            "address": {
+                "street": "Hoeger Mall",
+                "suite": "Apt. 692",
+                "city": "South Elvis",
+                "zipcode": "53919-4257",
+                "geo": {
+                    "lat": "29.4572",
+                    "lng": "-164.2990"
+                }
+            },
+            "phone": "493-170-9623 x156",
+            "website": "kale.biz",
+            "company": {
+                "name": "Robel-Corkery",
+                "catchPhrase": "Multi-tiered zero tolerance productivity",
+                "bs": "transition cutting-edge web services"
+            }
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "username": "Kamren",
+            "email": "Lucio_Hettinger@annie.ca",
+            "address": {
+                "street": "Skiles Walks",
+                "suite": "Suite 351",
+                "city": "Roscoeview",
+                "zipcode": "33263",
+                "geo": {
+                    "lat": "-31.8129",
+                    "lng": "62.5342"
+                }
+            },
+            "phone": "(254)954-1289",
+            "website": "demarco.info",
+            "company": {
+                "name": "Keebler LLC",
+                "catchPhrase": "User-centric fault-tolerant solution",
+                "bs": "revolutionize end-to-end systems"
+            }
+        },
+        {
+            "id": 6,
+            "name": "Mrs. Dennis Schulist",
+            "username": "Leopoldo_Corkery",
+            "email": "Karley_Dach@jasper.info",
+            "address": {
+                "street": "Norberto Crossing",
+                "suite": "Apt. 950",
+                "city": "South Christy",
+                "zipcode": "23505-1337",
+                "geo": {
+                    "lat": "-71.4197",
+                    "lng": "71.7478"
+                }
+            },
+            "phone": "1-477-935-8478 x6430",
+            "website": "ola.org",
+            "company": {
+                "name": "Considine-Lockman",
+                "catchPhrase": "Synchronised bottom-line interface",
+                "bs": "e-enable innovative applications"
+            }
+        },
+        {
+            "id": 7,
+            "name": "Kurtis Weissnat",
+            "username": "Elwyn.Skiles",
+            "email": "Telly.Hoeger@billy.biz",
+            "address": {
+                "street": "Rex Trail",
+                "suite": "Suite 280",
+                "city": "Howemouth",
+                "zipcode": "58804-1099",
+                "geo": {
+                    "lat": "24.8918",
+                    "lng": "21.8984"
+                }
+            },
+            "phone": "210.067.6132",
+            "website": "elvis.io",
+            "company": {
+                "name": "Johns Group",
+                "catchPhrase": "Configurable multimedia task-force",
+                "bs": "generate enterprise e-tailers"
+            }
+        },
+        {
+            "id": 8,
+            "name": "Nicholas Runolfsdottir V",
+            "username": "Maxime_Nienow",
+            "email": "Sherwood@rosamond.me",
+            "address": {
+                "street": "Ellsworth Summit",
+                "suite": "Suite 729",
+                "city": "Aliyaview",
+                "zipcode": "45169",
+                "geo": {
+                    "lat": "-14.3990",
+                    "lng": "-120.7677"
+                }
+            },
+            "phone": "586.493.6943 x140",
+            "website": "jacynthe.com",
+            "company": {
+                "name": "Abernathy Group",
+                "catchPhrase": "Implemented secondary concept",
+                "bs": "e-enable extensible e-tailers"
+            }
+        },
+        {
+            "id": 9,
+            "name": "Glenna Reichert",
+            "username": "Delphine",
+            "email": "Chaim_McDermott@dana.io",
+            "address": {
+                "street": "Dayna Park",
+                "suite": "Suite 449",
+                "city": "Bartholomebury",
+                "zipcode": "76495-3109",
+                "geo": {
+                    "lat": "24.6463",
+                    "lng": "-168.8889"
+                }
+            },
+            "phone": "(775)976-6794 x41206",
+            "website": "conrad.com",
+            "company": {
+                "name": "Yost and Sons",
+                "catchPhrase": "Switchable contextually-based project",
+                "bs": "aggregate real-time technologies"
+            }
+        },
+        {
+            "id": 10,
+            "name": "Clementina DuBuque",
+            "username": "Moriah.Stanton",
+            "email": "Rey.Padberg@karina.biz",
+            "address": {
+                "street": "Kattie Turnpike",
+                "suite": "Suite 198",
+                "city": "Lebsackbury",
+                "zipcode": "31428-2261",
+                "geo": {
+                    "lat": "-38.2386",
+                    "lng": "57.2232"
+                }
+            },
+            "phone": "024-648-3804",
+            "website": "ambrose.net",
+            "company": {
+                "name": "Hoeger LLC",
+                "catchPhrase": "Centralized empowering task-force",
+                "bs": "target end-to-end models"
+            }
+        }
+    ]
+
+
+
+
+
+
+function createItems() {
+    for (let i = 0; i < arr.length; i++) {
+        let itemHtml = `
+        <div class="item" id="${arr[i].id}" >
+        <h1>${arr[i].id}</h1>
+        <h1 class="title">${arr[i].name}</h1>
+        <p class="txt1">Company: <span>${arr[i].company.name}</span></p>
+        <p class="txt1">Website: <span>${arr[i].website}</span></p>
+        <p class="txt1">Phone: <span>${arr[i].phone}</span></p>
+        <button data-action id="${arr[i].id}" >Подробнее</button>
+      </div>
+        `
+        content.insertAdjacentHTML("beforeend", itemHtml)
+    }
+
+
+}
+
+createItems()
+
+
+window.addEventListener("click", (e) => {
+    if(e.target.hasAttribute("data-action")) {
+        console.log("click");
+        let id = e.target.id
+
+        let a = arr.filter(item => item.id == id)
+
+        localStorage.setItem('item', JSON.stringify(a))
+
+        // console.log(id);
+        // console.log(a);
+    }   
+})
+
+// const cliked = (e) => {
+//         console.log(e);
+
+// }
+
+// function cliked(e) {
+    
+// }
